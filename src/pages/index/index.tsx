9cdecb9a0fd0276a8c8c9189b74dbe04c239f870
Jairwin <jairwin@jairwin.cn>
2023-03-16 16:41:26 +0800

    init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,22 @@
+import { View } from "@tarojs/components";
+import { Button } from "@nutui/nutui-react-taro";
+import "./index.scss";
+
+export default function Main() {
+  return (
+    <>
+      <View className='main'>首页</View>
+      <Button type='primary' className='btn'>
+        主要按钮
+      </Button>
+      <View>
+        {Array(50)
+          .fill("1")
+          .map((item) => {
+            return <View key={item}>{item}</View>;
+          })}
+      </View>
+      <View>底部</View>
+    </>
+  );
+}
