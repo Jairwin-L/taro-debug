31da46ca15790f381ae08924e52a8d222ec6794e
Jairwin <jairwin@jairwin.cn>
2023-11-28 22:50:30 +0800

    chore: test icon

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,8 +1,7 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index',
     'pages/test/index',
-		'pages/other/index',
+    'pages/index/index',
   ],
   window: {
     backgroundTextStyle: 'light',
@@ -15,18 +14,18 @@ export default defineAppConfig({
     selectedColor: '#1677ff',
     backgroundColor: '#fff',
     list: [
-      {
-        pagePath: 'pages/index/index',
-        selectedIconPath: 'images/tabbar_main_selected.png',
-        iconPath: 'images/tabbar_main.png',
-        text: '首页',
-      },
-      {
-        pagePath: 'pages/test/index',
+			{
+				pagePath: 'pages/test/index',
         selectedIconPath: 'images/tabbar_mine_selected.png',
         iconPath: 'images/tabbar_mine.png',
         text: '测试',
       },
+			{
+				pagePath: 'pages/index/index',
+				selectedIconPath: 'images/tabbar_main_selected.png',
+				iconPath: 'images/tabbar_main.png',
+				text: '首页',
+			},
     ],
   },
 })
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,9 +1,17 @@
-import './index.scss';
+import { IconFont } from "@nutui/icons-react-taro";
+import "./index.scss";
 
 export default function Test() {
   return (
     <>
       测试
+      <IconFont
+        color='#fa2c19'
+        size='20'
+        fontClassName='my-icon'
+        classPrefix='icon'
+        name='test-js'
+      />
     </>
   );
 }
